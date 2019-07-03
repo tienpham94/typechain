9ce6479184ce76bd9411df1a8eb4b8f941f724e1
Tien Pham <tiendev94@gmail.com>
2019-07-03 21:53:30 +0300

    creating block

--- src/index.ts
+++ src/index.ts
@@ -34,10 +34,30 @@ const genesisBlock: Block = new Block(0, "20202020202", "", "Hello", 123456);
 
 let blockchain: [Block] = [genesisBlock];
 
-const getBlockchain = () : Block[] => blockchain
+const getBlockchain = (): Block[] => blockchain;
 
-const getLatestBlock = () : Block => blockchain[blockchain.length - 1]
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
-const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000)
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
-console.log(blockchain);
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock('hello'), createNewBlock('byeee'))
